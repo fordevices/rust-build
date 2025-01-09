537d8ba85b612c2e6eaafaee8db9ba1d9f69eea8
fordevices-vm <fordevices@outlook.com>
2025-01-09 13:00:57 -0500

    After making post quantum Key exchange mechanism working

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,40 @@
-// This a sample, derived out of the public sample from rustls public example
-// This TLS client will use Post Quantum Crypto
-// This is how this (or any) TLS Client will work
-// First setup a local cert store
-// fill it up with "known" root certs - a good source is Mozilla's Root certs
-// https://hg.mozilla.org/releases/mozilla-beta/file/tip/security/nss/lib/ckfw/builtins/certdata.txt
-// filter it down to certs that do not need mutual auth
-// emable SSLKEYLOGFILE for wireshark debugging later on
-// if : You want to decrypt and examine SSL application data using the SSL session key.
-//      You want to log the SSL session keys on the client system
-//      You are using Firefox or Chrome or Edge browsers client to access web application.
-//      https://my.f5.com/manage/s/article/K50557518
-
-// Rust projects are more readable when they stick to the standard project layout, 
-// which is auto-detected by Cargo. If you have just one main.rs
-// To do this, make your source file have the name src/main.rs 
-// instead of src/<your own name>.rs,
-// In Cargo.toml just say the name of the package you want
-// Leave out the [[bin]] section and path entirely from your Cargo.toml. 
-// The built executable will still be named Guessi<your own name>ngGame 
-// automatically, because that's your package name.
+//! This is the simplest possible client using rustls-postquantum, based on
+//! `simpleclient.rs`.
+//!
+//! It sends a HTTP request to pq.cloudflareresearch.com and prints the response to
+//! stdout.  Observe in that output: `kex=X25519MLKEM768`
+//!
+//! Note that `unwrap()` is used to deal with networking errors; this is not something
+//! that is sensible outside of example code.
 
 use std::io::{stdout, Read, Write};
 use std::net::TcpStream;
 use std::sync::Arc;
-use rustls::RootCertStore;
 
 fn main() {
-    let root_store = RootCertStore {
+    env_logger::init();
+    rustls_post_quantum::provider()
+         .install_default()
+         .unwrap();
+
+    let root_store = rustls::RootCertStore {
         roots: webpki_roots::TLS_SERVER_ROOTS.into(),
     };
-    let mut config = rustls::ClientConfig::builder()
+
+    let config = rustls::ClientConfig::builder()
         .with_root_certificates(root_store)
         .with_no_client_auth();
 
-    // Allow using SSLKEYLOGFILE.
-    config.key_log = Arc::new(rustls::KeyLogFile::new());
-
-    let server_name = "www.rust-lang.org".try_into().unwrap();
+    let server_name = "pq.cloudflareresearch.com"
+        .try_into()
+        .unwrap();
     let mut conn = rustls::ClientConnection::new(Arc::new(config), server_name).unwrap();
-    let mut sock = TcpStream::connect("www.rust-lang.org:443").unwrap();
+    let mut sock = TcpStream::connect("pq.cloudflareresearch.com:443").unwrap();
     let mut tls = rustls::Stream::new(&mut conn, &mut sock);
     tls.write_all(
         concat!(
-            "GET / HTTP/1.1\r\n",
-            "Host: www.rust-lang.org\r\n",
+            "GET /cdn-cgi/trace HTTP/1.0\r\n",
+            "Host: pq.cloudflareresearch.com\r\n",
             "Connection: close\r\n",
             "Accept-Encoding: identity\r\n",
             "\r\n"
@@ -61,7 +52,16 @@ fn main() {
         ciphersuite.suite()
     )
     .unwrap();
-//    let mut plaintext = Vec::new();
-//    tls.read_to_end(&mut plaintext).unwrap();
-//    stdout().write_all(&plaintext).unwrap();
+    let kx_group = tls
+        .conn
+        .negotiated_key_exchange_group()
+        .unwrap();
+    writeln!(
+        &mut std::io::stderr(),
+        "Current key exchange group: {kx_group:?}",
+    )
+    .unwrap();
+    let mut plaintext = Vec::new();
+    tls.read_to_end(&mut plaintext).unwrap();
+    stdout().write_all(&plaintext).unwrap();
 }
